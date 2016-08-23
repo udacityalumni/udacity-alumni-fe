70d455ec8cbbd66e79e22e1c8711627d59fecd98
Ryan Collins <rccproductions@mac.com>
2016-08-23 17:11:14 -0400

    Fix: utilize props in index.js

--- app/src/pages/LandingPage/index.js
+++ app/src/pages/LandingPage/index.js
@@ -8,7 +8,7 @@ import styles from './index.module.scss';
 // Handler that maps to a route in /utils/routes
 const LandingPage = (props) => (
   <div className={styles.container}>
-    // Code me
+    {...props}
   </div>
 );
 
