f5f0447387ab7b263d8cbc4b33ea772425dfc120
Ryan Collins <rccproductions@mac.com>
2016-10-01 23:38:47 -0400

    Feat: add production bundle

--- app/src/components/MainCarousel/index.js
+++ app/src/components/MainCarousel/index.js
@@ -1,20 +1,31 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import styles from './index.module.scss';
 import cssModules from 'react-css-modules';
 import Carousel from 'grommet/components/Carousel';
 import Box from 'grommet/components/Box';
 
-const MainCarousel = ({
-  images,
-}) => (
-  <Box className={styles.carouselBox}>
-    <Carousel>
-      {images.map((item, i) =>
-        <img key={i} src={item.src} />
-      )}
-    </Carousel>
-  </Box>
-);
+class MainCarousel extends Component {
+  componentDidMount() {
+     // HACK: call the resize event to make the carousel fit images
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 500);
+  }
+  render() {
+    const {
+      images,
+    } = this.props;
+    return (
+      <Box className={styles.carouselBox}>
+        <Carousel>
+          {images.map((item, i) =>
+            <img key={i} src={item.src} />
+          )}
+        </Carousel>
+      </Box>
+    );
+  }
+}
 
 MainCarousel.propTypes = {
   images: PropTypes.array.isRequired,
