73ce4d3fd4d8ec8d5a403883c10a68b29d744ae6
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-10-04 12:18:06 -0500

    chore: add reducer to root reduser

--- app/src/reducers.js
+++ app/src/reducers.js
@@ -7,6 +7,7 @@ import landing from 'containers/LandingContainer/reducer';
 import loginContainer from 'containers/LoginContainer/reducer';
 import signupContainer from 'containers/SignupContainer/reducer';
 import cmsEditorContainer from 'containers/CmsEditorContainer/reducer';
+import SinglePostReducer from 'containers/SinglePostContainer/reducer';
 
 const rootReducer = combineReducers({
   // Apply all of the reducers here.
@@ -14,6 +15,7 @@ const rootReducer = combineReducers({
   signupContainer,
   landing,
   cmsEditorContainer,
+  SinglePostReducer,
   routing: routerReducer,
   form: formReducer,
 });
