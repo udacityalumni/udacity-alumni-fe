c4d0d7bd92d74578b4ebc7e126b82ffbb284dab5
Martin <breuss.martin@gmail.com>
2016-10-10 16:15:53 +0200

    fix bug when choosing random article

--- app/src/containers/NotFoundContainer/index.js
+++ app/src/containers/NotFoundContainer/index.js
@@ -19,49 +19,49 @@ class NotFound extends Component { // eslint-disable-line react/prefer-stateless
   renderContent() {
     // https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
     function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+      return Math.floor(Math.random() * (max - min)) + min;
     }
 
     // a list of content entries that can be loop over randomly
     const content = [
       {
-        title: 'We could not think of enough banana bread recipes...',
+        title: 'We could not think of enough banana bread recipes',
         body: 'Learn to make an app that shuffles ingredients endlessly!',
         link: 'https://www.udacity.com/course/ios-developer-nanodegree--nd003',
         description: 'iOS Developer Nanodegree',
       },
       {
-        title: 'We did not manage to reticulate all splines...',
-        body: 'Learn to reticulate inputs to make a car spin its splines autonomously!',
+        title: 'We did not manage to reticulate all splines',
+        body: 'Learn to reticulate inputs to make cars spin their splines autonomously!',
         link: 'https://www.udacity.com/drive',
         description: 'Self-Driving Car Nanodegree',
       },
       {
-        title: 'The instructors are not quite camera-ready...',
+        title: 'The instructors are not quite camera-ready',
         body: 'Learn to make them look good anyways!',
         link: 'https://www.udacity.com/course/senior-web-developer-nanodegree-by-google--nd802',
         description: 'Senior Web-Developer Nanodegree',
       },
       {
-        title: 'None of the classrooms are currently prepared...',
+        title: 'None of the classrooms are currently prepared',
         body: 'Learn to create a virtual classroom that is always ready (and needs no brooming)!',
         link: 'https://www.udacity.com/course/vr-developer-nanodegree--nd017',
         description: 'Virtual-Reality Nanodegree',
       },
       {
-        title: 'This corner of the App did not get attention yet...',
+        title: 'This corner of the App is still empty',
         body: 'Bring it into existence (and learn a lot on the way)!',
         link: 'https://github.com/udacityalumni/',
         description: 'Contribute to the Alumni-Web-App',
       },
       {
-        title: 'Your code here...',
+        title: 'Your code here',
         body: 'If it is empty and you want it, come and make it!',
         link: 'https://github.com/udacityalumni/',
         description: 'Contribute to the Alumni-Web-App',
       },
       {
-        title: 'Fill this space with your work...',
+        title: 'Fill this space with your ideas',
         body: 'Become a part of the Developer-Team!',
         link: 'https://github.com/udacityalumni/',
         description: 'Contribute to the Alumni-Web-App',
@@ -73,22 +73,21 @@ class NotFound extends Component { // eslint-disable-line react/prefer-stateless
 
     return (
       <Section align="center">
-        <Header justify="center" direction="column">
+        <Header className="introduction" justify="center" direction="column">
           <Heading tag="h3">
             Hello curious adventurer! : )
           </Heading>
-          <Paragraph>
-            You've come to uncharted territory. 404-Land. Right were you belong! Ready to map?
+          <Paragraph alignContent="center">
+            You've come to uncharted territory. 404-Land. - What? Right were you belong, you say? Then get ready to map!
           </Paragraph>
         </Header>
-        <hr />
-        <Heading tag="h1" className="legacy-message">
+        <Heading tag="h1" className="problem">
           { content[randomArticle].title }
         </Heading>
-        <Paragraph className="learn-more">
+        <Paragraph className="solution">
           { content[randomArticle].body }
         </Paragraph>
-        <Anchor href={ content[randomArticle].link }>
+        <Anchor className="solution-link" href={ content[randomArticle].link }>
           { content[randomArticle].description }
         </Anchor>
       </Section>
