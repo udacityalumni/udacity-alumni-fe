a278d8939b3357fef9536fbce658396959d84ed6
Martin <breuss.martin@gmail.com>
2016-10-10 17:40:33 +0200

    add all original loading messages and 404 alterations

--- app/src/containers/NotFoundContainer/index.js
+++ app/src/containers/NotFoundContainer/index.js
@@ -25,43 +25,92 @@ class NotFound extends Component { // eslint-disable-line react/prefer-stateless
     // a list of content entries that can be loop over randomly
     const content = [
       {
+        original: 'Contemplating banana bread recipes',
         title: 'We could not think of enough banana bread recipes',
         body: 'Learn to make an app that shuffles ingredients endlessly!',
         link: 'https://www.udacity.com/course/ios-developer-nanodegree--nd003',
         description: 'iOS Developer Nanodegree',
       },
       {
+        original: 'Reticulating Splines',
         title: 'We did not manage to reticulate all splines',
         body: 'Learn to reticulate inputs to make cars spin their splines autonomously!',
         link: 'https://www.udacity.com/drive',
         description: 'Self-Driving Car Nanodegree',
       },
       {
+        original: 'The instructors are getting camera-ready for you',
         title: 'The instructors are not quite camera-ready',
         body: 'Learn to make them look good anyways!',
         link: 'https://www.udacity.com/course/senior-web-developer-nanodegree-by-google--nd802',
         description: 'Senior Web-Developer Nanodegree',
       },
       {
-        title: 'None of the classrooms are currently prepared',
+        original: 'The instructor is setting up the classroom',
+        title: 'None of the classrooms are currently set up',
         body: 'Learn to create a virtual classroom that is always ready (and needs no brooming)!',
         link: 'https://www.udacity.com/course/vr-developer-nanodegree--nd017',
         description: 'Virtual-Reality Nanodegree',
       },
       {
-        title: 'This corner of the App is still empty',
-        body: 'Bring it into existence (and learn a lot on the way)!',
+        original: 'Adding duplication fluid to our ditto machine...',
+        title: 'Ran out of duplication fluid',
+        body: 'Learn to code and make functions do the work!',
+        link: 'https://www.udacity.com/course/intro-to-programming-nanodegree--nd000',
+        description: 'Introduction to Programming Nanodegree',
+      },
+      {
+        original: 'Once you have completed the course, cake will be served',
+        title: 'There should be some cake here for you.',
+        body: 'Learn to make digital cake that almost smells home-made!',
+        link: 'https://www.udacity.com/course/vr-developer-nanodegree--nd017',
+        description: 'Virtual-Reality Nanodegree',
+      },
+      {
+        original: 'Dust off your slide-rule...',
+        title: 'No need for dusty slide-rules',
+        body: 'Learn how to make them virtually!',
+        link: 'https://www.udacity.com/course/vr-developer-nanodegree--nd017',
+        description: 'Virtual-Reality Nanodegree',
+      },
+      {
+        original: 'Cleaning the whiteboard',
+        title: 'Clean up this white board',
+        body: 'And fill it up with useful information!',
         link: 'https://github.com/udacityalumni/',
         description: 'Contribute to the Alumni-Web-App',
       },
       {
+        original: 'And now for something completely different...',
+        title: 'And now for something slightly different...',
+        body: 'Learn about Business Analysis because, you know... money.',
+        link: 'https://www.udacity.com/course/predictive-analytics-for-business--nd008',
+        description: 'Predictive Analytics for Business Nanodegree',
+      },
+      {
+        original: 'Check the materials section for each class to find helpful course notes and other information from fellow Udacians!',
+        title: 'Check out the wiki for this project to find helpful notes from fellow Alumni!',
+        body: 'There is enough to read to fill a rainy day!',
+        link: 'https://github.com/udacityalumni/resources/wiki',
+        description: 'Learn about the Alumni-Web-App',
+      },
+      {
+        original: 'Did you know you can meet other Udacians on the Forums?',
+        title: 'Did you know you can meet other Alumnis through contributing?',
+        body: 'Bring this page into existence (and learn a lot on the way)!',
+        link: 'https://github.com/udacityalumni/',
+        description: 'Contribute to the Alumni-Web-App',
+      },
+      {
+        original: 'Thanks for waiting while we prepare the classroom',
         title: 'Your code here',
         body: 'If it is empty and you want it, come and make it!',
         link: 'https://github.com/udacityalumni/',
         description: 'Contribute to the Alumni-Web-App',
       },
       {
-        title: 'Fill this space with your ideas',
+        original: 'Waking up the sleepers in the front row...',
+        title: 'Hei, front-row-sleeper: Put your dreams on this page',
         body: 'Become a part of the Developer-Team!',
         link: 'https://github.com/udacityalumni/',
         description: 'Contribute to the Alumni-Web-App',
@@ -77,8 +126,8 @@ class NotFound extends Component { // eslint-disable-line react/prefer-stateless
           <Heading tag="h3">
             Hello curious adventurer! : )
           </Heading>
-          <Paragraph alignContent="center">
-            You've come to uncharted territory. 404-Land. - What? Right were you belong, you say? Then get ready to map!
+          <Paragraph textAlign="center">
+            You've come to uncharted territory. 404-Land. - Is that right were you belong? Then get ready to map!
           </Paragraph>
         </Header>
         <Heading tag="h1" className="problem">
