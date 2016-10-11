7aba5b3a11a130ee828388333f6cf1b225f378f5
Andreas Daiminger <andreas.daiminger@gmail.com>
2016-10-11 18:22:05 +0200

    feat: implement very basic search term highlighting

--- app/src/components/ArticlePreview/index.js
+++ app/src/components/ArticlePreview/index.js
@@ -2,17 +2,22 @@ import React, { PropTypes } from 'react';
 import Anchor from 'grommet-udacity/components/Anchor';
 import Box from 'grommet-udacity/components/Box';
 import Card from 'grommet-udacity/components/Card';
-
+import { highlightTitle, highlightContent } from './utils';
 
 const ArticlePreview = ({
   article,
-}) => (
-  <Box size="medium" pad={{ vertical: 'medium' }}>
+  searchTerm,
+}) => {
+  // const formattedTitle = highlightTitle(searchTerm, article.title);
+  // const formattedLabel = highlightTitle(searchTerm, article.user.name);
+  const formattedDescription = highlightContent(searchTerm, article.content);
+  return (
+    <Box size="medium" pad={{ vertical: 'medium' }}>
     <Card
       label={`By ${article.user.name}`}
       thumbnail={article.feature_image}
       heading={article.title.slice(0, 15)}
-      description={`${article.content.slice(0, 200)}...`}
+      description={formattedDescription}
       link={
         <Anchor
           href={`/articles/${article.id}`}
@@ -22,7 +27,8 @@ const ArticlePreview = ({
       }
     />
   </Box>
-);
+  );
+};
 
 ArticlePreview.propTypes = {
   article: PropTypes.shape({
@@ -32,6 +38,7 @@ ArticlePreview.propTypes = {
     feature_image: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
+    searchTerm: PropTypes.searchTerm,
   }),
 };
 
---
+++ app/src/components/ArticlePreview/utils/index.js
@@ -0,0 +1,20 @@
+// helper to insert mark tags
+function insertMarkTag(sourceString, subString) {
+  return sourceString.replace(new RegExp("(" + subString + ")", "g"), "<mark>$1</mark>");
+}
+
+// {`${article.content.slice(0, 200)}...`}
+export const highlightContent = (searchTerm, text) => {
+  if (text.indexOf(searchTerm) !== -1) {
+    /**
+      TODO: do a better algorythm to get the right text section containing
+      the search term. Right now we only show the first 200 characters of the text.
+      This will be a problem for bigger texts.
+    **/
+    const formattedText = insertMarkTag(text, searchTerm);
+    return formattedText.slice(0, 200);
+  }
+  else {
+    return text.slice(0, 200);
+  }
+};
