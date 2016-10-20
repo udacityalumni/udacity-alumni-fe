20929dd6c5c9525936a399c10ee9861b01bd163d
Ryan Collins <rccproductions@mac.com>
2016-10-19 20:42:59 -0400

    Feat: update content dashboard to handle deletion of articls

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,4 +1,5 @@
 /* Assemble all components for export */
+export ConfirmationModal from './ConfirmationModal';
 export SingleArticleContent from './SingleArticleContent';
 export CarouselWidget from './CarouselWidget';
 export CmsEditorPreview from './CmsEditorPreview';
