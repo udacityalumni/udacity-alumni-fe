5e7c11ef2fc4f99966e22b58e6a24fd221f3bc15
Ryan Collins <rccproductions@mac.com>
2016-10-21 15:52:40 -0400

    Feat: remove the dev server and command, now that start will get hot module replacemente running

--- app/src/components/MainCarousel/index.js
+++ app/src/components/MainCarousel/index.js
@@ -3,23 +3,30 @@ import styles from './index.module.scss';
 import cssModules from 'react-css-modules';
 import Carousel from 'grommet-udacity/components/Carousel';
 import Box from 'grommet-udacity/components/Box';
+import Animate from 'grommet-udacity/components/Animate';
 
-class MainCarousel extends Component {
+class MainCarousel extends Component {  // eslint-disable-line react/prefer-stateless-function
   render() {
     const {
       images,
     } = this.props;
     return (
       <Box className={styles.carouselBox}>
-        <Carousel>
-          {images && images.map((item, i) =>
-            <img
-              style={{ maxWidth: '100vw', maxHeight: 300, minWidth: 850, width: 'auto' }}
-              key={i}
-              src={item.url}
-            />
-          )}
-        </Carousel>
+        <Animate
+          visible={images && images.length > 0}
+          enter={{ animation: 'fade', duration: 2500 }}
+          keep
+        >
+          <Carousel>
+            {images && images.map((item, i) =>
+              <img
+                style={{ maxWidth: '100vw', maxHeight: 300, minWidth: 850, width: 'auto' }}
+                key={i}
+                src={item.url}
+              />
+            )}
+          </Carousel>
+        </Animate>
       </Box>
     );
   }
