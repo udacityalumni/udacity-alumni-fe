7f10d00d9f5e72fdd42d999542ed7bcfeb083ccb
kellim <kelli.blalock2@gmail.com>
2016-10-20 23:46:41 -0700

    Add Loading Message Data
    Message data for loading copied from NotFoundContainer

---
+++ app/src/components/LoadingIndicator/data/messageData.js
@@ -0,0 +1,11 @@
+// Loading Indicator messages to be looped over randomly
+
+const messageData = [
+  'Contemplating banana bread recipes...',
+  'Reticulating Splines...',
+  'Adding duplication fluid to our ditto machine...',
+  'Dusting off your slide-rule...',
+  'Cleaning the whiteboard...',
+];
+
+export default messageData;
