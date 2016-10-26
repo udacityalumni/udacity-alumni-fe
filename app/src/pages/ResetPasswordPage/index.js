07d26ade9ae6f77fb921b2d3f7bd1657667c564f
Ryan Collins <rccproductions@mac.com>
2016-10-26 14:32:43 -0400

    Feat: update production build

--- app/src/pages/ResetPasswordPage/index.js
+++ app/src/pages/ResetPasswordPage/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import cssModules from 'react-css-modules';
 import styles from './index.module.scss';
 import { ResetPasswordContainer } from 'containers';
+import { AppFooter } from 'components';
 
 const ResetPasswordPage = ({
   location,
 }) => (
   <div className={styles.container}>
     <ResetPasswordContainer location={location} />
+    <AppFooter />
   </div>
 );
 
