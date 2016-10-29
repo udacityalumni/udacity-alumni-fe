106b5a57d837903dfe9734b1e5f2748e94329b99
Martin <breuss.martin@gmail.com>
2016-10-29 23:20:16 +0200

    feat: mb add carousel

---
+++ app/src/pages/AboutPage/contributorVoices.js
@@ -0,0 +1,13 @@
+const contributorVoices = [
+  {
+    src: 'https://placehold.it/350x150',
+  },
+  {
+    src: 'https://placehold.it/200x100',
+  },
+  {
+    src: 'https://placehold.it/350x150',
+  }
+];
+
+export default contributorVoices;
--- app/src/pages/AboutPage/index.js
+++ app/src/pages/AboutPage/index.js
@@ -4,7 +4,12 @@ import styles from './index.module.scss';
 import Heading from 'grommet-udacity/components/Heading';
 import Hero from 'grommet-udacity/components/Hero';
 import Headline from 'grommet-udacity/components/Headline';
-import { MartinRulz, AppFooter } from 'components';
+import { MartinRulz,
+        AppFooter,
+        MainCarousel } from 'components';
+// importing the Udacity loading messages and their adapted versions
+
+import contributorVoices from './contributorVoices';
 // Change as needed.  This image is optimized
 const AboutPageImage = 'https://github.com/RyanCCollins/cdn/blob/master/alumni-webapp/meetups.jpeg?raw=true';
 
@@ -21,8 +26,19 @@ const AboutPage = () => (
       </Heading>
     </Hero>
     <MartinRulz />
+    <MainCarousel
+      images={[
+        {
+          src: 'http://oi63.tinypic.com/24vse9f.jpg',
+        },
+        {
+          src: 'http://oi63.tinypic.com/24vse9f.jpg',
+        }
+      ]}
+    />
     <AppFooter />
   </div>
 );
+
 
 export default cssModules(AboutPage, styles);
