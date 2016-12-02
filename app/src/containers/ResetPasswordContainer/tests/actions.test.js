7c2ca909a69023e8b1f37906d4f59b47040f649c
Shuyu <shuyu@hanmini.local>
2016-12-02 14:47:09 -0600

    Test: fix test error on action creators

--- app/src/containers/ResetPasswordContainer/tests/actions.test.js
+++ app/src/containers/ResetPasswordContainer/tests/actions.test.js
@@ -3,12 +3,38 @@ import * as actions from '../actions';
 import * as types from '../constants';
 
 describe('ResetPassword actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
-      const expected = {
-        type: types.RESETPASSWORD_DEFAULT_ACTION,
-      };
-      expect(actions.resetPasswordDefaultAction()).toEqual(expected);
-    });
-  });
+	it('has a type of RESET_PASSWORD_REQUEST_INITIATION', () => {
+	  const expected = {
+	    type: types.RESET_PASSWORD_REQUEST_INITIATION,
+	  };
+	  expect(actions.resetPasswordRequestInitiation()).toEqual(expected);
+	});
+	it('has a type of RESET_PASSWORD_REQUEST_SUCCESS', () => {
+		const message = 'password reset request succeeded!';
+	  const expected = {
+	    type: types.RESET_PASSWORD_REQUEST_SUCCESS,
+	    message,
+	  };
+	  expect(actions.resetPasswordRequestSuccess(message)).toEqual(expected);
+	});
+	it('has a type of RESET_PASSWORD_REQUEST_FAILURE', () => {
+		const error = new Error('An error has occured');
+	  const expected = {
+	    type: types.RESET_PASSWORD_REQUEST_FAILURE,
+	    error,
+	  };
+	  expect(actions.resetPasswordRequestFailure(error)).toEqual(expected);
+	});
+	it('has a type of RESET_PASSWORD_CLEAR_ERROR', () => {
+	  const expected = {
+	    type: types.RESET_PASSWORD_CLEAR_ERROR,
+	  };
+	  expect(actions.resetPasswordClearError()).toEqual(expected);
+	});
+	it('has a type of RESET_PASSWORD_CLEAR_MESSAGE', () => {
+	  const expected = {
+	    type: types.RESET_PASSWORD_CLEAR_MESSAGE,
+	  };
+	  expect(actions.resetPasswordClearMessage()).toEqual(expected);
+	});
 });
