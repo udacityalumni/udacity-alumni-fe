634c51170d4af3269c39589b53c0fe744d03c529
ghoshabhi <abhighosh18@gmail.com>
2016-12-13 15:38:03 -0800

    Style: Style the feedback form to show either submission message or the form

--- app/src/components/AddFeedbackForm/index.js
+++ app/src/components/AddFeedbackForm/index.js
@@ -42,111 +42,118 @@ class AddFeedbackForm extends Component {
     } = this.props;
     return (
       <div>
-        {message &&
+        {message ?
           <Heading
             tag="h4"
             align="center"
           >
-            <Paragraph>
+            <Paragraph
+              size="large"
+              className={styles.strong}
+            >
               {message}
             </Paragraph>
           </Heading>
+        :
+          <div>
+            <Headline
+              size="small"
+              strong
+            >
+              We, Thank You For Your Feedback!
+            </Headline>
+
+            <Heading
+              tag="h4"
+              align="center"
+            >
+              Please Fill Out The Form Below...
+            </Heading>
+            <Form>
+              <FormFields>
+                <FormField
+                  label="Name *"
+                  htmlFor="feedbackNameInput"
+                  help="Please make sure your name is correct or Edit your profile to correct it!"
+                  error={nameInput.touched && nameInput.error ? nameInput.error : null}
+                >
+                  <input
+                    {...nameInput}
+                    ref="nameInput"
+                    className={styles.blockedInput}
+                    id="feedbackNameInput"
+                    type="text"
+                    name="name"
+                    className={styles.blockedInput}
+                    disabled
+                    required
+                  />
+                </FormField>
+                <FormField
+                  label="URL *"
+                  htmlFor="feedbackUrlnput"
+                  help="Fill in the URL you want to give feedback about"
+                  error={urlInput.touched && urlInput.error ? urlInput.error : null}
+                >
+                  <input
+                    {...urlInput}
+                    ref="urlInput"
+                    className={styles.longUrl}
+                    id="urlInput"
+                    type="text"
+                    name="url"
+                    disabled
+                    required
+                  />
+                </FormField>
+                <FormField
+                  label="Description"
+                  htmlFor="feedbackDescriptionInput"
+                  help="Describe your problem/feedback"
+                  error={descriptionInput.touched && descriptionInput.error
+                        ?
+                          descriptionInput.error
+                        :
+                          null
+                        }
+                >
+                  <textarea
+                    {...descriptionInput}
+                    id="feedbackDescriptionInput"
+                    ref="feedbackDescriptionInput"
+                    type="text"
+                    rows="5"
+                    cols="40"
+                    required
+                  />
+                </FormField>
+              </FormFields>
+              <Footer className={styles.footer}>
+                {user ?
+                  <Menu direction="row">
+                    <Button
+                      className={styles.button}
+                      label="Submit"
+                      primary
+                      disabled={isSubmitting ? true : false}
+                      onClick={onSubmitFeedback}
+                    />
+                    <Button
+                      label="Clear"
+                      onClick={onClear}
+                    />
+                  </Menu>
+                :
+                  <Heading
+                    tag="h3"
+                  >
+                    Please Login to contiue giving us your feedback :)
+                  </Heading>
+                }
+              </Footer>
+            </Form>
+          </div>
         }
-        <Headline
-          size="small"
-          strong
-        >
-          We, Thank You For Your Feedback!
-        </Headline>
-        <Heading
-          tag="h4"
-          align="center"
-        >
-          Please Fill Out The Form Below...
-        </Heading>
-        <Form>
-          <FormFields>
-            <FormField
-              label="Name *"
-              htmlFor="feedbackNameInput"
-              help="Please make sure your name is correct or Edit your profile to correct it!"
-              error={nameInput.touched && nameInput.error ? nameInput.error : null}
-            >
-              <input
-                {...nameInput}
-                ref="nameInput"
-                className={styles.blockedInput}
-                id="feedbackNameInput"
-                type="text"
-                name="name"
-                className={styles.blockedInput}
-                disabled
-                required
-              />
-            </FormField>
-            <FormField
-              label="URL *"
-              htmlFor="feedbackUrlnput"
-              help="Fill in the URL you want to give feedback about"
-              error={urlInput.touched && urlInput.error ? urlInput.error : null}
-            >
-              <input
-                {...urlInput}
-                ref="urlInput"
-                className={styles.longUrl}
-                id="urlInput"
-                type="text"
-                name="url"
-                disabled
-                required
-              />
-            </FormField>
-            <FormField
-              label="Description"
-              htmlFor="feedbackDescriptionInput"
-              help="Describe your problem/feedback"
-              error={descriptionInput.touched && descriptionInput.error
-                    ?
-                      descriptionInput.error
-                    :
-                      null
-                    }
-            >
-              <textarea
-                {...descriptionInput}
-                id="feedbackDescriptionInput"
-                ref="feedbackDescriptionInput"
-                type="text"
-                rows="5"
-                cols="40"
-                required
-              />
-            </FormField>
-          </FormFields>
-          <Footer className={styles.footer}>
-            {user ?
-              <Menu direction="row">
-                <Button
-                  className={styles.button}
-                  label="Submit"
-                  primary
-                  disabled={isSubmitting ? true : false}
-                  onClick={onSubmitFeedback}
-                />
-                <Button
-                  label="Clear"
-                  onClick={onClear}
-                />
-              </Menu>
-            :
-              <Heading
-                tag="h3"
-              >
-                Please Login to contiue giving us your feedback :)
-              </Heading>
-            }
-          </Footer>
-        </Form>
       </div>
     );
   }
