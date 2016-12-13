a0729e7b100a90c5441a8a541b3d927aadb4686d
ghoshabhi <abhighosh18@gmail.com>
2016-12-13 10:39:11 -0800

    Style: Fix indentation

--- app/src/components/AddFeedbackForm/index.js
+++ app/src/components/AddFeedbackForm/index.js
@@ -36,6 +36,7 @@ class AddFeedbackForm extends Component {
       urlInput,
       descriptionInput,
       onSubmitFeedback,
+      isSubmitting,
       onClear,
       user,
       message,
@@ -52,97 +53,101 @@ class AddFeedbackForm extends Component {
             </Paragraph>
           </Heading>
         }
-      <Headline
-        size="small"
-        strong
-      >
-        We, Thank You For Your Feedback!
-      </Headline>
-      <Heading
-        tag="h4"
-        align="center"
-      >
-        Please Fill Out The Form Below...
-      </Heading>
-      <Form>
-        <FormFields>
-          <FormField
-            label="Name *"
-            htmlFor="feedbackNameInput"
-            help="Please make sure your name is correct or Edit your profile to correct it!"
-            error={nameInput.touched && nameInput.error ? nameInput.error : null}
-          >
-            <input
-              {...nameInput}
-              ref="nameInput"
-              className={styles.blockedInput}
-              id="feedbackNameInput"
-              type="text"
-              name="name"
-              className={styles.blockedInput}
-              disabled
-              required
-            />
-          </FormField>
-          <FormField
-            label="URL *"
-            htmlFor="feedbackUrlnput"
-            help="Fill in the URL you want to give feedback about"
-            error={urlInput.touched && urlInput.error ? urlInput.error : null}
-          >
-            <input
-              {...urlInput}
-              ref="urlInput"
-              className={styles.longUrl}
-              id="urlInput"
-              type="text"
-              name="url"
-              disabled
-              required
-            />
-          </FormField>
-          <FormField
-            label="Description"
-            htmlFor="feedbackDescriptionInput"
-            help="Describe your problem/feedback"
-            error={descriptionInput.touched && descriptionInput.error
-               ? descriptionInput.error : null
+        <Headline
+          size="small"
+          strong
+        >
+          We, Thank You For Your Feedback!
+        </Headline>
+        <Heading
+          tag="h4"
+          align="center"
+        >
+          Please Fill Out The Form Below...
+        </Heading>
+        <Form>
+          <FormFields>
+            <FormField
+              label="Name *"
+              htmlFor="feedbackNameInput"
+              help="Please make sure your name is correct or Edit your profile to correct it!"
+              error={nameInput.touched && nameInput.error ? nameInput.error : null}
+            >
+              <input
+                {...nameInput}
+                ref="nameInput"
+                className={styles.blockedInput}
+                id="feedbackNameInput"
+                type="text"
+                name="name"
+                className={styles.blockedInput}
+                disabled
+                required
+              />
+            </FormField>
+            <FormField
+              label="URL *"
+              htmlFor="feedbackUrlnput"
+              help="Fill in the URL you want to give feedback about"
+              error={urlInput.touched && urlInput.error ? urlInput.error : null}
+            >
+              <input
+                {...urlInput}
+                ref="urlInput"
+                className={styles.longUrl}
+                id="urlInput"
+                type="text"
+                name="url"
+                disabled
+                required
+              />
+            </FormField>
+            <FormField
+              label="Description"
+              htmlFor="feedbackDescriptionInput"
+              help="Describe your problem/feedback"
+              error={descriptionInput.touched && descriptionInput.error
+                    ?
+                      descriptionInput.error
+                    :
+                      null
+                    }
+            >
+              <textarea
+                {...descriptionInput}
+                id="feedbackDescriptionInput"
+                ref="feedbackDescriptionInput"
+                type="text"
+                rows="5"
+                cols="40"
+                required
+              />
+            </FormField>
+          </FormFields>
+          <Footer className={styles.footer}>
+            {user ?
+              <Menu direction="row">
+                <Button
+                  className={styles.button}
+                  label="Submit"
+                  primary
+                  disabled={isSubmitting ? true : false}
+                  onClick={onSubmitFeedback}
+                />
+                <Button
+                  label="Clear"
+                  onClick={onClear}
+                />
+              </Menu>
+            :
+              <Heading
+                tag="h3"
+              >
+                Please Login to contiue giving us your feedback :)
+              </Heading>
             }
-          >
-            <textarea
-              {...descriptionInput}
-              id="feedbackDescriptionInput"
-              ref="feedbackDescriptionInput"
-              type="text"
-              rows="5"
-              cols="40"
-              required
-            />
-          </FormField>
-        </FormFields>
-        <Footer className={styles.footer}>
-          {user ?
-          <Menu direction="row">
-            <Button
-              className={styles.button}
-              label="Submit"
-              primary
-              onClick={onSubmitFeedback}
-            />
-            <Button
-              label="Clear"
-              onClick={onClear}
-            />
-          </Menu>
-          :
-          <Heading
-            tag="h3"
-          >
-            Please Login to contiue giving us your feedback :)
-          </Heading>
-          }
-        </Footer>
-      </Form>
+          </Footer>
+        </Form>
       </div>
     );
   }
@@ -155,6 +160,7 @@ AddFeedbackForm.propTypes = {
   onSubmitFeedback: PropTypes.func.isRequired,
   onClear: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
+  isSubmitting: PropTypes.bool.isRequired,
   message: PropTypes.string,
 };
 
